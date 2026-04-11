22c5ac74886d55bbb20a3a83a8af22af1b8e9e61
Youssef Essam <youssef21042007@gmail.com>
2026-04-11 15:11:32 +0200

    Added Employee Class made it inherit from Person

--- Hospital System/Employee.cs
+++ Hospital System/Employee.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Hospital_System
 {
-	internal class Employee
-	{
-	}
+	internal class Employee: Person
+    {
+		private decimal salary;
+        public decimal Salary
+        {
+            get => salary;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Salary cannot be negative and must be greater than zero.");
+                    salary = value;
+            }
+        }
+        public Employee(string name, string surname, int age, char gender, string id, string phoneNumber, decimal salary)
+            : base(name, surname, age, gender, id, phoneNumber)
+        {
+            this.Salary = salary;
+        }
+    }
 }
