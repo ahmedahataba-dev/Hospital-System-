17886ae543b6b465b68692e01b71ec7900773525
Youssef Essam <youssef21042007@gmail.com>
2026-04-13 17:06:24 +0200

    fixed all errors

--- Hospital System/Program.cs
+++ Hospital System/Program.cs
@@ -15,22 +15,22 @@ namespace Hospital_System
             //Person p1 = new Person("ahe", 50, GenderType.Male, "00701181200491", "01025920863");
             //System.Windows.Forms.MessageBox.Show($"phone no ={p1.PhoneNumber}");
             //System.Windows.Forms.MessageBox.Show($"id ={p1.NationalId}");
-            Employee e1 = new Employee("ahmed", 19, GenderType.Male, "30701181200491", "01025920863","youssef@gmail.com","egypt",5000, 7, 16, 3);
-            Employee e2 = new Employee("sayed", 19, GenderType.Male, "30701181200491", "01025920863", "youssef@gmail.com", "egypt", 5000, 7, 16, 3);
+            //Employee e1 = new Employee("ahmed", 19, GenderType.Male, "30701181200491", "01025920863","youssef@gmail.com","egypt",5000, 7, 16, 3);
+            //Employee e2 = new Employee("sayed", 19, GenderType.Male, "30701181200491", "01025920863", "youssef@gmail.com", "egypt", 5000, 7, 16, 3);
             //Employee e3 = new Employee("yed", 19, GenderType.Male, "30701181200491", "01025920863", 5000, 7, 16.5, 3);
 
-            System.Windows.Forms.MessageBox.Show($"id ={e1.EmployeeId}");
-            System.Windows.Forms.MessageBox.Show($"id ={e2.EmployeeId}");
+            //System.Windows.Forms.MessageBox.Show($"id ={e1.EmployeeId}");
+            //System.Windows.Forms.MessageBox.Show($"id ={e2.EmployeeId}");
             //System.Windows.Forms.MessageBox.Show($"id ={e3.EmployeeId}");
 
             // Create a Department on Floor 2
-            Department surgery = new Department("Surgery","2");
+            /*Department surgery = new Department("Surgery","2");
 
             // Create a Room that says it's on Floor 5 (Opps!)
             Room wrongRoom = new Room("501", "Operating Theater", 5);
 
             // This will trigger your "Error" message!
-            surgery.AddRoom(wrongRoom);
+            surgery.AddRoom(wrongRoom);*/
 
         }
 	}
