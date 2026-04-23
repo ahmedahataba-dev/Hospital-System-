e95c3d9d071db8e3935af0153446f953ff2f0fb6
ahmedahataba-dev <ahmeda.hataba@gmail.com>
2026-04-23 14:41:19 +0200

    Enhance Employee class by adding deduction calculation
    Refactor Employee class for improved clarity and functionality.

--- Hospital System/Employee.cs
+++ Hospital System/Employee.cs
@@ -1,138 +1,65 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;//using static System.Windows.Forms.VisualStyles.VisualStyleElement;// By Ahmed Hatabanamespace Hospital_System
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
+
 namespace Hospital_System
 {
 	internal class Employee : Person
 	{
 		public static int employeeid_counter = 1;
-		private decimal salary;//basic salary if there is no deductions 
-		private DateTime checkintime;
+		private decimal salary; // Basic salary
+		private DateTime checkintime; 
 		private DateTime checkouttime;
 		private TimeSpan workedhours;
 		private TimeSpan lateness;
-		private TimeSpan shiftstart = new TimeSpan(9, 0, 0);
+		private TimeSpan shiftstart = new TimeSpan(9, 0, 0); //shift starts at 9
 		private double experienceyears;
 		private int employeeid;
-		private bool ischeckedin;
-		//list includes All employees
+		
 		public static List<Employee> employees = new List<Employee>();
 
+		// Properties
+		public int EmployeeId
+		{
+			get { return employeeid; }
+			set { if (value <= 0) throw new ArgumentException("Invalid ID."); else employeeid = value; }
+		}
 
-		//      public double ArrivalTime {
-		//	get { return arrivaltime; }
-		//	set
-		//	{
-		//		if (value <= 0)
-		//		{
-		//			throw new ArgumentException("Invalid Time .");
-		//		}
-		//              else{ 
+		public decimal Salary
+		{
+			get => salary;
+			set { if (value <= 0) throw new ArgumentException("Invalid Salary."); salary = value; }
+		}
 
-        public int EmployeeId
-        {
-            get { return employeeid; }
-            set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Invalid ID .");
-                }
-                else
-                {
-                    employeeid = value;
-                }
-            }
-        }
-
-        public decimal Salary
-        {
-            get => salary;
-            set
-            {
-                if (value <= 0) throw new ArgumentException("Invalid Salary .");
-                salary = value;
-            }
-        }
-
-        public Employee(string name, int age, GenderType gender, string Nationalid, string phoneNumber, string email, string address, decimal salary, double experienceyears/*, int employeeid*/)
-            : base(name, age, gender, Nationalid, phoneNumber, email, address)
-        {
-            Salary = salary;
-            ExperienceYears = experienceyears;
-            EmployeeId = employeeid_counter;
-            employeeid_counter++;
-        }
-
-        public Employee(string name)
-        {
-            Name = name;
-        }
-
-        public void CheckIn()
-        {
-            if (!ischeckedin)
-            {
-                this.checkintime = DateTime.Now;
-                ischeckedin = true;
-            }
-            else Console.WriteLine("You Are Already Checked In .");
-        }
-
-        public void CheckOut()
-        {
-            if (ischeckedin)
-            {
-                this.checkouttime = DateTime.Now;
-                this.workedhours = checkouttime - checkintime;
-                Console.WriteLine($"On {this.checkintime:dd-MM-yyyy}\n{this.Name} Checked In At :{this.checkintime:hh:mm:ss tt}	|	" +
-                $"Checked Out At :{this.checkouttime:hh:mm:ss tt}\n Good Bye .");
-                ischeckedin = false;
-            }
-            else Console.WriteLine("Please Check In first .");
-        }
-
-        public void CheckInandOut(Employee emp)
-        {
-            Console.Write($"Please Enter Your ID {this.Name} : ");
-            Console.WriteLine($"Hello {this.Name} Do You Want To Check In Or out \nChoose 1-Check IN				2-Check Out  ");
+		public double ExperienceYears
+		{
+			get { return experienceyears; }
+			set { if (value < 0) throw new ArgumentException("Invalid Number."); experienceyears = value; }
+		}
 
 		public bool IsCheckedIn { get; set; }
 		public DateTime CheckInTime { get; set; }
-		//public decimal NetSalary
-		//{
-		//	get => netsalary;
-		//	set
-		//	{
-		//		if (value <= 0) throw new ArgumentException("Invalid Salary .");
-		//		netsalary = value;
-		//	}
-		//}
-		public decimal TotalDeductionAmmount { set; get; }
-
+		public decimal TotalDeductionAmmount { get; set; }
+		
 		public decimal NetSalary => Salary - TotalDeductionAmmount;
 
-
-		public Employee(string name, int age, GenderType gender, string Nationalid, string phoneNumber, string email, string address, decimal salary, double experienceyears/*, int employeeid*/)
+		public Employee(string name, int age, GenderType gender, string Nationalid, string phoneNumber, string email, string address, decimal salary, double experienceyears)
 			: base(name, age, gender, Nationalid, phoneNumber, email, address)
 		{
 			Salary = salary;
 			ExperienceYears = experienceyears;
-			//employees.Add(this);
+			EmployeeId = employeeid_counter;
+			employeeid_counter++;
 			HospitalData.AddEmployee(this);
-
 		}
 
-
-
-
-
+		// ميثود الـ ValidateId عشان تربط الـ UI بالـ Backend
 		static public void ValidateId(int id)
 		{
-
 			var emp = employees.FirstOrDefault(e => e.EmployeeId == id);
 			if (emp != null)
 			{
@@ -144,7 +71,6 @@ namespace Hospital_System
 			}
 		}
 
-
 		public void CheckIn()
 		{
 			if (!IsCheckedIn)
@@ -153,22 +79,20 @@ namespace Hospital_System
 				IsCheckedIn = true;
 				decimal dailydeductionammount = 0;
 
-
 				lateness = (CheckInTime.TimeOfDay) - shiftstart;
 				if (lateness > TimeSpan.Zero)
 				{
 					dailydeductionammount = (salary / (30 * 8 * 60)) * (decimal)lateness.TotalMinutes;
 					TotalDeductionAmmount += dailydeductionammount;
-					//netsalary = salary - totaldeductionammount;
 					Console.WriteLine($"You Are Late {lateness.TotalMinutes:f0} Minutes");
 				}
 				else
 				{
 					Console.WriteLine("You Arrived On Time . Well Done!");
 				}
+				HospitalData.SaveEmployees(); 
 			}
 			else Console.WriteLine("You Are Already Checked In .");
-			HospitalData.SaveEmployees();//saves the public attributes in the "employee.json" file
 		}
 
 		public void CheckOut()
@@ -177,41 +101,26 @@ namespace Hospital_System
 			{
 				this.checkouttime = DateTime.Now;
 				this.workedhours = checkouttime - CheckInTime;
-				Console.WriteLine($"On {this.CheckInTime:dd-MM-yyyy}\n{this.Name} Checked In At :{this.CheckInTime:hh:mm:ss tt}	|	" +
+				Console.WriteLine($"On {this.CheckInTime:dd-MM-yyyy}\n{this.Name} Checked In At :{this.CheckInTime:hh:mm:ss tt} | " +
 				$"Checked Out At :{this.checkouttime:hh:mm:ss tt}\nGood Bye .");
 				IsCheckedIn = false;
+				HospitalData.SaveEmployees();
 			}
 			else Console.WriteLine("Please Check In first .");
 		}
 
 		public void CheckInandOut()
 		{
-			//Console.Write($"Please Enter Your ID {this.Name} : ");
-			Console.WriteLine($"Hello {this.Name} Do You Want To Check In Or out \nChoose 1-Check IN				2-Check Out  ");
-
-			if (int.TryParse(Console.ReadLine(), out int chosenno)) //try parse to avoid wrong input crash 
+			Console.WriteLine($"Hello {this.Name}\nChoose 1-Check IN | 2-Check Out");
+			if (int.TryParse(Console.ReadLine(), out int chosenno))
 			{
 				switch (chosenno)
 				{
-					case 1:
-						CheckIn();
-						Console.WriteLine("Checked In Successfully");
-						break;
-					case 2:
-						CheckOut();
-						Console.WriteLine($"Total Time Worked: {this.workedhours.Hours}h {this.workedhours.Minutes}m");
-						break;
-					default:
-						Console.WriteLine("Invalid Number Choosen");
-						break;
+					case 1: CheckIn(); break;
+					case 2: CheckOut(); break;
+					default: Console.WriteLine("Invalid Number Chosen"); break;
 				}
 			}
-
-			/*this.checkouttime = DateTime.Now;
-			this.workedhours = checkouttime - checkintime;
-			Console.WriteLine($"On {this.checkintime:dd-MM-yyyy}\n{this.Name} Checked In At :{this.checkintime:hh:mm:ss}	|	" +
-			$"Checked Out At :{this.checkouttime:hh:mm:ss}");
-	*/
 		}
 	}
 }
